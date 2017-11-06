8b7dffc18b83288d2be958dcf7aa42febda8d0d3
espen <espenk95@gmail.com>
2017-11-06 14:26:43 +0100

    La til drive() (med test kode i loop()).

--- roger2.ino
+++ roger2.ino
@@ -1,12 +1,16 @@
-#include <ZumoMotors.h>
-
 /*
 
 roger2
 
 */
 
+#include <ZumoMotors.h>
+
+
 const int PIN_LED = 13;
+
+
+enum Direction {Forward, Backward, Left, Right, SwivelLeft, SwivelRight};
 
 
 ZumoMotors motor;
@@ -18,20 +22,48 @@ void setup() {
 
 
 void loop() {
-    int motorSpeed = 200;
-    motor.setLeftSpeed(motorSpeed);
-    motor.setRightSpeed(motorSpeed);
-    delay(3000);
-    motorSpeed = 0;
-    motor.setLeftSpeed(motorSpeed);
-    motor.setRightSpeed(motorSpeed);
-    motorSpeed = -400;
-    motor.setLeftSpeed(motorSpeed);
-    motor.setRightSpeed(motorSpeed);
     delay(2000);
-    motorSpeed = 0;
-    motor.setLeftSpeed(motorSpeed);
-    motor.setRightSpeed(motorSpeed);
-    delay(6000);
+    drive(200, Forward);
+    delay(2000);
+    drive(200, Backward);
+    delay(2000);
+    drive(200, Left);
+    delay(2000);
+    drive(400, Right);
+    delay(2000);
+    drive(200, SwivelLeft);
+    delay(2000);
+    drive(400, SwivelRight),
+    delay(2000);
 }
 
+
+void drive(int speed, Direction direction) {
+    speed = constrain(speed, -400, 400);
+    switch (direction) {
+        case Forward:
+            motor.setLeftSpeed(speed);
+            motor.setRightSpeed(speed);
+            break;
+        case Backward:
+            motor.setLeftSpeed(-speed);
+            motor.setRightSpeed(-speed);
+            break;
+        case Left:
+            motor.setLeftSpeed(speed * 0.4);
+            motor.setRightSpeed(speed);
+            break;
+        case Right:
+            motor.setLeftSpeed(speed);
+            motor.setRightSpeed(speed * 0.4);
+            break;
+        case SwivelLeft:
+            motor.setLeftSpeed(-speed);
+            motor.setRightSpeed(speed);
+            break;
+        case SwivelRight:
+            motor.setLeftSpeed(speed);
+            motor.setRightSpeed(-speed);
+        break;
+    }
+}
