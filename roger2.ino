61eb00759fa748b51461834cb297db473d364631
espen <espenk95@gmail.com>
2017-11-06 13:46:19 +0100

    Test av motor

--- roger2.ino
+++ roger2.ino
@@ -1,15 +1,37 @@
+#include <ZumoMotors.h>
+
 /*
 
 roger2
 
 */
 
-void setup() {
+const int PIN_LED = 13;
 
+
+ZumoMotors motor;
+
+
+void setup() {
+    pinMode(PIN_LED, OUTPUT);
 }
 
 
 void loop() {
-
+    int motorSpeed = 200;
+    motor.setLeftSpeed(motorSpeed);
+    motor.setRightSpeed(motorSpeed);
+    delay(3000);
+    motorSpeed = 0;
+    motor.setLeftSpeed(motorSpeed);
+    motor.setRightSpeed(motorSpeed);
+    motorSpeed = -400;
+    motor.setLeftSpeed(motorSpeed);
+    motor.setRightSpeed(motorSpeed);
+    delay(2000);
+    motorSpeed = 0;
+    motor.setLeftSpeed(motorSpeed);
+    motor.setRightSpeed(motorSpeed);
+    delay(6000);
 }
 
